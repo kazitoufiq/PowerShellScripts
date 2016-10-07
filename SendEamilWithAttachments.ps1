591fed0b2290763e37bc4583e78942e6b7df75c1
Kazi Toufiq Wadud <kazitoufiq@users.noreply.github.com>
2016-10-07 13:03:10 +1100

    Create SendEamilWithAttachments.ps1

---
+++ SendEamilWithAttachments.ps1
@@ -0,0 +1,4 @@
+send-mailmessage -to 'k.w@email.com' `
+-from 'myemail@email' -subject 'Multiple email attachments' `
+-SmtpServer 'outlook.xxxx.xx.x'  `
+-attachment 'C:\Users\kaz.csv', 'C:\Users\kazidd.csv'
